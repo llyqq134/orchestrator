62538b5f658f8687eead7217b97dd4b3fb8f6503
lly <llyqq134@gmail.com>
2025-12-15 14:04:34 +0300

    adding comms to the state

--- pkg/resources/task/state.go
+++ pkg/resources/task/state.go
@@ -12,9 +12,14 @@ Task can have 4 state:
 	failed
 */
 const (
+	// the initial state, the starting point for every task
 	Pending State = iota
+	// a task moves to this state once the manager has scheduled it onto a worker
 	Scheduled
+	// a task moves to this state when a worker successfully starts the tasks
 	Running
+	// a task moves to this state when is completes its work in a normal way
 	Completed
+	// if a task fails it moves to this state
 	Failed
 )
