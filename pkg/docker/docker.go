ec236438e2d0e5e4f30be9e7d397beeb367aa57f
lly <llyqq134@gmail.com>
2025-12-15 13:39:18 +0300

    added init func and renamed cpu field in task.Config struct

---
+++ pkg/docker/docker.go
@@ -0,0 +1,113 @@
+package docker
+
+import (
+	"context"
+	"io"
+	"log"
+	"math"
+	"orchestrator/pkg/resources/task"
+	"os"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/image"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+type Docker struct {
+	Client *client.Client
+	Config task.Config
+}
+
+type Result struct {
+	Error       error
+	Action      string
+	ContainerID string
+	Result      string
+}
+
+func NewDocker(c task.Config) Docker {
+	return Docker {
+		Config: c,
+	}
+}
+
+func (d *Docker) Run() Result {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(ctx, d.Config.Image, image.PullOptions{})
+	if err != nil {
+		log.Printf("Failed to pull image: %v", err)
+		return Result{Error: err}
+	}
+	io.Copy(os.Stdout, reader)
+
+	rp := container.RestartPolicy {
+		Name: container.RestartPolicyMode(d.Config.RestartPolicy),
+	}
+
+	r := container.Resources {
+		Memory: d.Config.Memory,
+		NanoCPUs: int64(d.Config.CPU * math.Pow(10, 9)),
+	}
+
+	cc := container.Config {
+		Image: d.Config.Image,
+		Tty: false,
+		Env: d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+
+	hc := container.HostConfig {
+		RestartPolicy: rp,
+		Resources: r,
+		PublishAllPorts: true,
+	}
+
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf("Error creating container using image %s: %v\n", d.Config.Image, err)
+		return Result{Error: err}
+	}
+
+	if err = d.Client.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
+		log.Printf("Error starting container %s: %v\n", resp.ID, err)
+		return Result{Error: err}
+	}
+	
+	out, err := d.Client.ContainerLogs(
+		ctx, 
+		resp.ID,
+		container.LogsOptions{ShowStdout: true, ShowStderr: true},
+	)
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", resp.ID, err)
+		return Result{Error: err}
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+
+	return Result {ContainerID: resp.ID, Action: "start", Result: "success"}
+}
+
+func (d *Docker) Stop(id string) Result {
+	log.Printf("Attempting to stop container %v", id )
+
+	if err := d.Client.ContainerStop(context.Background(), id, container.StopOptions{});
+		err != nil {
+			log.Printf("Error stopping container %s: %v\n", id, err)
+			return Result{Error: err}
+	}
+
+	err := d.Client.ContainerRemove(context.Background(), id, container.RemoveOptions{
+		RemoveVolumes: true,
+		RemoveLinks: false,
+		Force: false,
+	})
+
+	if err != nil {
+		log.Printf("Error removing container %s: %v\n", id, err)
+		return Result{Error: err}
+	}
+
+	return Result{Action: "stop", Result: "success", Error: nil}
+}
--- pkg/resources/task/config.go
+++ pkg/resources/task/config.go
@@ -10,9 +10,21 @@ type Config struct {
 	ExposedPorts  nat.PortSet
 	Cmd           []string
 	Image         string
-	Cpu           float64
+	CPU           float64
 	Memory        int64
 	Disk          int64
 	Env           []string
 	RestartPolicy string
+}
+
+func NewConfig(t Task) Config {
+	return Config {
+		Name: t.Name,
+		ExposedPorts: t.ExposedPorts,
+		Image: t.Image,
+		CPU: t.CPU,
+		Memory: int64(t.Memory),
+		Disk: int64(t.Disk),
+		RestartPolicy: t.RestartPolicy,
+	}	
 }
