45dbd4c9077125e066d5f8e985bd4bfafcee3942
llyqq134 <pinklywmp@gmail.com>
2026-05-12 16:05:18 +0300

    added init func, name for rr alg, params for methods

--- pkg/resources/scheduler/scheduler.go
+++ pkg/resources/scheduler/scheduler.go
@@ -1,7 +1,30 @@
 package scheduler
 
+import (
+	"orchestrator/pkg/resources/node"
+	roundrobin "orchestrator/pkg/resources/scheduler/roundRobin"
+	"orchestrator/pkg/resources/task"
+)
+
+const (
+	RoundRobinScheduler = "roundRobin"
+)
+
 type Scheduler interface {
-	SelectCandidateNodes()
-	Score()
-	Pick()
+	SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node
+	Score(t task.Task, nodes []*node.Node) map[string]float64
+	Pick(scores map[string]float64, nodes []*node.Node) *node.Node
+}
+
+func New(kind string) Scheduler {
+	switch kind {
+	case RoundRobinScheduler:
+		return &roundrobin.RoundRobin{
+			Name: RoundRobinScheduler,
+		}
+	default:
+		return &roundrobin.RoundRobin{
+			Name: RoundRobinScheduler,
+		}
+	}
 }
