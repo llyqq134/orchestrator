b4e83469209faef71211a2fde0f890ea50254b33
llyqq134 <pinklywmp@gmail.com>
2026-05-12 22:49:53 +0300

    rearch and moved getstats to nodes

--- pkg/resources/node/node.go
+++ pkg/resources/node/node.go
@@ -1,6 +1,15 @@
 package node
 
-import "orchestrator/pkg/metrics"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"orchestrator/pkg/metrics"
+	"orchestrator/pkg/utils"
+)
 
 type Node struct {
 	Name            string
@@ -22,4 +31,37 @@ func New(name, api, role string) *Node {
 		Api:  api,
 		Role: role,
 	}
+}
+
+func (n *Node) GetStats() (*metrics.Stats, error) {
+	op := "[node.GetStats]: "
+
+	url := fmt.Sprintf("http://%s/stats", n.Api)
+	resp, err := utils.HTTPWithRetry(http.Get, url)
+	if err != nil {
+		msg := fmt.Sprintf("Unable to connect to %v. Permanent failure", n.Api)
+		log.Println(op + msg)
+
+		return nil, errors.New(msg)
+	}
+
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("Error retrieving stats from %v: %v", n.Api, err)
+		log.Println(op + msg)
+
+		return nil, errors.New(msg)
+	}
+
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var stats metrics.Stats
+	if err = json.Unmarshal(body, &stats); err != nil {
+		msg := fmt.Sprintf("Error decoding message while getting stats for node %v", n.Name)
+		log.Println(op + msg)
+
+		return nil, err
+	}
+
+	return &n.Stats, nil
 }
