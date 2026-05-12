fe4882528f5c546cd9ef0c3850e94a225af00879
llyqq134 <pinklywmp@gmail.com>
2026-05-12 22:23:06 +0300

    added new fields

--- pkg/resources/node/node.go
+++ pkg/resources/node/node.go
@@ -1,22 +1,25 @@
 package node
 
+import "orchestrator/pkg/metrics"
+
 type Node struct {
 	Name            string
 	IpAddr          string
-	Api   					string 
+	Api             string
 	Cores           int
 	Memory          int
 	MemoryAllocated int
 	Disk            int
 	DiskAllocated   int
+	Stats           metrics.Stats
 	Role            string
 	TaskCount       int
 }
 
 func New(name, api, role string) *Node {
-	return &Node {		
-		Name: name,	
-		Api: api,
+	return &Node{
+		Name: name,
+		Api:  api,
 		Role: role,
 	}
 }
